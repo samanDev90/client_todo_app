cbe79953cdd020461037b5c6496a7c08325e02f7
samanDev90 <samanDeveloper90s@gmail.com>
2024-11-15 14:43:05 -0500

    Created Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddTaskHeader from "../layouts/AddTaskHeader";
+
+const Home = () => {
+  return (
+    <>
+      <AddTaskHeader />
+    </>
+  );
+};
+
+export default Home;
